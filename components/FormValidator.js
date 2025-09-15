3432fb73ad62994114a03c3590751c29da6c1561
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2025-09-15 07:09:05 -0500

    formValidtor 2

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -5,9 +5,40 @@ class FormValidator {
     this._errorClass = settings.errorClass;
     this._inputErrorClass = settings.inputErrorClass;
     this._inactiveButtonClass = settings.inactiveButtonClass;
-    this._formElement = formEl;
+    this._formEl = formEl;
   }
 
-  enableValidation() {}
+  //Todo - implement all of the other methods
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(inputElement);
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    const buttonElement = formElement.querySelector(this._submitButtonSelector);
+
+    toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
 }
 export default FormValidator;
